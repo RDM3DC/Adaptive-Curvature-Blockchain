ff45c1fc65653bbd9a06987b60820efa1c83bb89
Ryan mckenna <75239678+RDM3DC@users.noreply.github.com>
2025-05-05 00:50:08 -0700

    Create node.py

---
+++ src/node.py
@@ -0,0 +1,19 @@
+from adaptive_curvature import AdaptiveCurvature
+from blockchain import Blockchain, Block
+import time
+
+class Node:
+    def __init__(self):
+        self.blockchain = Blockchain()
+        self.curvature = AdaptiveCurvature()
+
+    def perform_optimization(self, problem_complexity):
+        satisfaction_score = np.random.uniform(0, problem_complexity)
+        unsatisfaction_penalty = problem_complexity - satisfaction_score
+        curvature = self.curvature.update_curvature(satisfaction_score, unsatisfaction_penalty)
+        return curvature
+
+    def create_block(self, data):
+        latest_block = self.blockchain.get_latest_block()
+        new_block = Block(latest_block.index + 1, latest_block.hash, time.time(), data)
+        self.blockchain.add_block(new_block)
